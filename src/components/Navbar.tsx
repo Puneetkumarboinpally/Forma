8148c331861557910dfe28477f51d4fef2c8db24
puneerkumarboinpally <109670302+Puneetkumarboinpally@users.noreply.github.com>
2026-08-15 20:35:28 +0100

    navigation links added

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
-
+import { NavLink } from "react-router-dom";
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <div>
+      <div>
+        <img src="" alt="Brand Logo" />
+        <h2>FORMA</h2>
+      </div>
+      <div>
+        <NavLink to={"/"}>SHOP</NavLink>
+        <NavLink to={"/collection"}>COLLECTIONS</NavLink>
+        <NavLink to={"/living-room"}>LIVING ROOM</NavLink>
+        <NavLink to={"/bedroom"}>BEDROOM</NavLink>
+        <NavLink to={"/dinning"}>DINNING</NavLink>
+        <NavLink to={"/lighting"}>LIGHTING</NavLink>
+      </div>
+      <div>
+        
+      </div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
