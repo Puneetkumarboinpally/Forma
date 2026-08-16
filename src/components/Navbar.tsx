f3f6ebfb2e26440fa17190a9d2e397002af16cce
puneerkumarboinpally <109670302+Puneetkumarboinpally@users.noreply.github.com>
2026-08-16 02:25:29 +0100

    navbar logo added

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,21 +1,42 @@
+import { Moon, Search, ShoppingBag, Sun } from "lucide-react";
 import { NavLink } from "react-router-dom";
+import { useThemeStore } from "../store/themeStore";
+import { useEffect } from "react";
 const Navbar = () => {
+  const theme = useThemeStore((state) => state.theme);
+  const toggle = useThemeStore((state) => state.toggleTheme);
+
+  useEffect(() => {
+    document.documentElement.setAttribute("data-theme", theme);
+  }, [theme]);
   return (
-    <div>
-      <div>
-        <img src="" alt="Brand Logo" />
-        <h2>FORMA</h2>
+    <div className="flex justify-between items-center h-16 max-w-container p-4 bg-background border-b border-b-border">
+      <div className="flex gap-4 items-center">
+        <img
+          className="h-8 w-8 object-center"
+          src="/src/assets/logo.png"
+          alt="Brand Logo"
+        />
+        <h2 className="text-2xl font-bold">FORMA</h2>
       </div>
-      <div>
-        <NavLink to={"/"}>SHOP</NavLink>
-        <NavLink to={"/collection"}>COLLECTIONS</NavLink>
-        <NavLink to={"/living-room"}>LIVING ROOM</NavLink>
-        <NavLink to={"/bedroom"}>BEDROOM</NavLink>
-        <NavLink to={"/dinning"}>DINNING</NavLink>
-        <NavLink to={"/lighting"}>LIGHTING</NavLink>
+      <div className="flex gap-4">
+        <NavLink to={"/"} className="nav-link">SHOP</NavLink>
+        <NavLink to={"/collection"} className="nav-link">COLLECTIONS</NavLink>
+        <NavLink to={"/living-room"} className="nav-link">LIVING ROOM</NavLink>
+        <NavLink to={"/bedroom"} className="nav-link">BEDROOM</NavLink>
+        <NavLink to={"/dinning"} className="nav-link">DINNING</NavLink>
+        <NavLink to={"/lighting"} className="nav-link">LIGHTING</NavLink>
       </div>
-      <div>
-        
+      <div className="flex gap-4">
+        <div>
+          <Search />
+        </div>
+        <div>
+          <ShoppingBag />
+        </div>
+        <button onClick={toggle}>
+          {theme === "dark" ? <Sun /> : <Moon />}
+        </button>
       </div>
     </div>
   );
