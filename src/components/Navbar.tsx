d4aad818737feb1bb0a77abcf0e5eeb6d5fb6ee8
puneerkumarboinpally <109670302+Puneetkumarboinpally@users.noreply.github.com>
2026-08-17 02:37:38 +0100

    nav menu completed

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,11 @@
-import { Moon, Search, ShoppingBag, Sun } from "lucide-react";
+import { Menu, Moon, Search, ShoppingBag, Sun, X } from "lucide-react";
 import { useThemeStore } from "../store/themeStore";
-import { useEffect } from "react";
-import NavMenu from "./NavMenu";
-import MobileMenu from "./MobileMenu";
-import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { links } from "../data/NavLinks";
+import { Link, NavLink } from "react-router-dom";
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
   const theme = useThemeStore((state) => state.theme);
   const toggle = useThemeStore((state) => state.toggleTheme);
 
@@ -13,33 +14,85 @@ const Navbar = () => {
   }, [theme]);
   return (
     <div className="flex justify-between items-center h-16 max-w-container p-4 bg-background border-b border-b-border">
-      {/* LOGO CONTAINER */}
-
+      {/* --- NAVBAR LOGO SECTION ---*/}
       <div className="flex items-center">
-        <Link to={"/"} className="text-xl font-bold">
+        <Link
+          to={"/"}
+          className="text-3xl font-extrabold text-accent font-display"
+        >
           FORMA
         </Link>
-        {/* NAV MENU CONTAINER */}
-      </div>
-      <div>
-        <NavMenu />
       </div>
 
-      {/* NAVBAR CTA CONTAINER */}
+      {/* --- NAV MENU SECTION ---*/}
+      <nav className="flex gap-4 max-laptop:gap-2">
+        {links.map((link) => (
+          <NavLink
+            key={link.id}
+            to={link.path}
+            className={({ isActive }) =>
+              `nav-link-styles max-laptop:hidden ${isActive ? "text-clay" : "text-secondary"}`
+            }
+          >
+            {link.name}
+          </NavLink>
+        ))}
+      </nav>
 
-      <div className="flex gap-4 tablet:gap-2">
+      {/* --- NAVIGATION CTA SECTION ---*/}
+
+      <div className="flex items-center gap-4">
+        {/* --- SEARCH SECTION ---*/}
         <div>
-          <Search />
+          <button aria-label="search button">
+            <Search />
+          </button>
         </div>
+
+        {/* --- CART SECTION ---*/}
         <div>
-          <ShoppingBag />
+          <button aria-label="cart button">
+            <ShoppingBag />
+          </button>
         </div>
-        <button onClick={toggle}>
-          {theme === "dark" ? <Sun /> : <Moon />}
+
+        {/* --- DARK THEME TOGGLE BUTTON ---*/}
+        <button
+          onClick={toggle}
+          aria-label={
+            theme === "dark" ? "Switch to light mode" : "switch to dark mode"
+          }
+          className="cursor-pointer border rounded-full p-1"
+        >
+          {theme === "dark" ? <Sun size={20} /> : <Moon size={20} />}
         </button>
-        {/* ---- */}
-        <div>
-          <MobileMenu />
+
+        {/* --- MOBILE MENU NAVIGATION ---*/}
+        <div className="relative laptop:hidden">
+          <button
+            onClick={() => setIsOpen((prev) => !prev)}
+            aria-label={isOpen ? "close menu" : "open menu"}
+          >
+            {isOpen ? <X /> : <Menu />}
+          </button>
+          <nav
+            className={`fixed top-16 left-0 z-50 h-[calc(100vh-4rem)] w-full bg-foreground flex flex-col gap-4 p-6 transition-transform duration-500 ease-out ${
+              isOpen ? "translate-x-0" : "-translate-x-full"
+            }`}
+          >
+            {links.map((link) => (
+              <NavLink
+                key={link.id}
+                to={link.path}
+                onClick={() => setIsOpen(false)}
+                className={({ isActive }) =>
+                  `nav-link-styles ${isActive ? "text-accent" : "text-secondary"}`
+                }
+              >
+                {link.name}
+              </NavLink>
+            ))}
+          </nav>
         </div>
       </div>
     </div>
