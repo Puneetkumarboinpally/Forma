69432304e81c2972ea9aa4754db6079b61730f8b
puneerkumarboinpally <109670302+Puneetkumarboinpally@users.noreply.github.com>
2026-08-21 21:20:58 +0100

    collection page added

---
+++ src/pages/CollectionPage.tsx
@@ -0,0 +1,13 @@
+const CollectionPage = () => {
+  return (
+    <div className="h-screen">
+      <div>
+        <h1>All Furniture</h1>
+        <p>Thoughtfully designed pieces for every room in your home.</p>
+      </div>
+      
+    </div>
+  );
+};
+
+export default CollectionPage;
