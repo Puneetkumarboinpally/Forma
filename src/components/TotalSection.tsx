4cb54670c6a048575a95db9c763f788e6e190890
puneerkumarboinpally <109670302+Puneetkumarboinpally@users.noreply.github.com>
2026-08-30 22:24:28 +0100

    order summary functionality added

--- src/components/TotalSection.tsx
+++ src/components/TotalSection.tsx
@@ -1,30 +1,44 @@
 import { BadgeCheck, LockKeyhole, RefreshCw, Truck } from "lucide-react";
+import { useCartStore } from "../store/cartStore";
 
 const TotalSection = () => {
+  const { cart } = useCartStore();
+
+  const subTotal = cart.reduce(
+    (total, item) => total + item.product.price * item.quantity,
+    0,
+  );
+  const shipping = subTotal < 1000 ? 45 : 0;
+  const taxes = subTotal * 0.2;
+  const discount = subTotal > 2000 ? subTotal * 0.1 : 0;
+  const total = subTotal + shipping + taxes - discount;
+
   return (
     <section className="h-full w-full laptop:w-[30rem] bg-surface p-8 laptop:sticky laptop:top-0">
       <h1 className="text-2xl font-body font-bold mb-6">Order Summary</h1>
       <div className="flex flex-col gap-4">
         <div className="order-grouped">
-          <h2>Subtotal</h2>
-          <p>£680.00</p>
+          <h2>SubTotal</h2>
+          <p>£{subTotal.toFixed(2)}</p>
         </div>
         <div className="order-grouped">
           <h2>Shipping Charge</h2>
-          <p>£45.00</p>
+          <p>£{shipping.toFixed(2)}</p>
         </div>
         <div className="order-grouped">
-          <h2>Taxes</h2>
-          <p>£45.00</p>
+          <h2>Tax(10%)</h2>
+          <p>£{taxes.toFixed(2)}</p>
         </div>
         <div className="order-grouped">
           <h2>Discount</h2>
-          <p>£0.00</p>
+          <p>£{discount.toFixed(2)}</p>
         </div>
         <div className="py-6 border-y border-border">
           <div className="flex justify-between items-center mb-2">
             <h2 className="text-3xl font-display font-bold">Total</h2>
-            <p className="text-2xl font-body font-semibold">£725.00</p>
+            <p className="text-2xl font-body font-semibold">
+              £{total.toFixed(2)}
+            </p>
           </div>
           <p className="font-medium font-body text-end mb-8 text-muted">
             Taxes included. Delivery calculated at checkout.
@@ -42,7 +56,7 @@ const TotalSection = () => {
             CHECKOUT SECURELY
             <LockKeyhole
               size={20}
-              className="font-bold transition-all
+              className="transition-transform
           duration-300 hover:bg-primary 
           group-hover:translate-x-2"
             />
